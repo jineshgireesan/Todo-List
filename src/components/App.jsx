3a83a3eb182b918ee50ef5747138b9f7e2345531
Jinesh Gireesan <jineshgireesan@Jineshs-MacBook-Air.local>
2024-02-09 00:35:28 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import TodoItem from "./TodoItem.jsx";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    setInputText(event.target.value);
+  }
+
+  function handleClick() {
+    setItems((prevValue) => {
+      return [...prevValue, inputText];
+    });
+    setInputText("");
+  }
+
+  function deleteItem(id) {
+    setItems((prevValue) => {
+      return prevValue.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />;
+        <button onClick={handleClick}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((todoItem, index) => (
+            <TodoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
